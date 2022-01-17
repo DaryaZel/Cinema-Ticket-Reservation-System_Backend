ab7bcfc1ffca13401077f4ca9052484cb54c44eb
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2022-01-17 15:59:12 +0300

    add id field to cinemaHallWithSeats response

--- CinemaHalls/cinemaHallService.js
+++ CinemaHalls/cinemaHallService.js
@@ -11,6 +11,9 @@ class CinemaHallService {
     async getHall(id) { 
         const foundCinemaHall = await CinemaHall.findById(id);
         const seats = await Seat.find({ hall_id: foundCinemaHall._id }).sort({ rowNumber: 1, number: 1 });
+        for (let i = 0; i < seats.length; i++) {
+            seats[i].id = seats[i]._id.toString()
+        }
         return { foundCinemaHall, seats };
     }
 
