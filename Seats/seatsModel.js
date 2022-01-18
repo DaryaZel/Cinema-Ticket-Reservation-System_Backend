4fdb0ff68cf36c2fbeae2a64a663ef9b414c673a
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2022-01-18 12:00:32 +0300

    add fields to seat model

--- Seats/seatsModel.js
+++ Seats/seatsModel.js
@@ -4,9 +4,11 @@ const Seat = new mongoose.Schema({
     hall_id: { type: mongoose.Schema.Types.ObjectId, ref: 'CinemaHall', required: true },
     number: { type: Number, required: true },
     rowNumber: { type: Number, required: true },
+    price: { type: Number, required: true },
     type: { type: String, required: true },
     tooltip: { type: String, required: true },
-    reserved: { type: Boolean, required: true },
+    isSelected: { type: Boolean, required: true },
+    isReserved: { type: Boolean, required: true },
     temporaryReserved: { type: Boolean, required: true }
 })
 
